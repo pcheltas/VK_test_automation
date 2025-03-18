93109536894f8a03ee9ca2c405cd83aaa62547a0
pcheltas <pcheltas@gmail.com>
2025-03-18 17:04:10 +0300

    initial commit

---
+++ src/main/java/org/pcheltas/Main.java
@@ -0,0 +1,7 @@
+package org.pcheltas;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Base test")
+public class BaseTest {
+    @Test
+    @DisplayName("Verify true is true ¯\\_(ツ)_/¯ ")
+    void baseTest(){
+        assertTrue(true);
+    }
+}
